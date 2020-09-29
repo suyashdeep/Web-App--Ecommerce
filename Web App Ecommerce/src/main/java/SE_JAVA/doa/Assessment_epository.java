d86a6954711ca6266a0bdcaa19425d58f3175c72
Suyash Deep Gupta <suyashd199@gmail.com>
2020-09-29 11:50:03 +0530

    Add files via upload

---
+++ Web App Ecommerce/src/main/java/SE_JAVA/doa/Assessment_epository.java
@@ -0,0 +1,8 @@
+package SE_JAVA.doa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import SE_JAVA.models.product_info;
+
+public interface Assessment_epository  extends JpaRepository<product_info, Integer>{
+
+}
---
+++ Web App Ecommerce/src/main/java/SE_JAVA/service/Assesment_service.java
@@ -0,0 +1,28 @@
+package SE_JAVA.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import SE_JAVA.doa.Assessment_epository;
+import SE_JAVA.models.product_info;
+
+@Service
+public class Assesment_service {
+	@Autowired
+	private Assessment_epository rep;
+
+	public Collection<product_info> findallproduct(){
+		List<product_info> pinfo= new ArrayList<product_info>();
+		for (product_info product_info : rep.findAll()) {
+			pinfo.add(product_info);
+			//System.out.println("id = " +product_info.getId());
+		}
+		
+		return pinfo;
+	}
+}
